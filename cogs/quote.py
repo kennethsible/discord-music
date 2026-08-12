38f7bb1d6e5dd1dcde2a8e415644538db02485d4
Ken Sible <ksible@outlook.com>
2026-08-12 16:52:24 -0400

    replaced word_tokenize for QuoteBot

--- cogs/quote.py
+++ cogs/quote.py
@@ -1,9 +1,8 @@
-from nltk.tokenize import word_tokenize
 from collections import Counter
 
 from discord import app_commands
 from discord.ext import commands
-import discord, random, json
+import discord, random, json, re
 
 class QuoteBot(commands.Cog):
 
@@ -11,15 +10,22 @@ class QuoteBot(commands.Cog):
         self.bot = bot
         with open('data/quotes.json') as quotes_file:
             self.quotes = Counter(json.load(quotes_file))
+        self.alias_map: dict[str, list[str]] = {}
+        for aliases, responses in self.quotes.items():
+            for alias in aliases.split(', '):
+                self.alias_map[alias.lower()] = responses
 
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.author == self.bot.user: return
-        for qbot in self.quotes:
-            for alias in qbot.split(', '):
-                if alias.lower() in word_tokenize(message.content.lower()):
-                    await message.channel.send(random.choice(self.quotes[qbot])
-                        .replace('<@>', f'<@{message.author.id}>'))
+        tokens = set(re.findall(r'\b\w+\b', message.content.lower()))
+        alias_match = tokens.intersection(self.alias_map)
+        if alias_match:
+            alias = next(iter(alias_match))
+            response = random.choice(self.alias_map[alias]).replace(
+                '<@>', f'<@{message.author.id}>'
+            )
+            await message.channel.send(response)
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(QuoteBot(bot))
