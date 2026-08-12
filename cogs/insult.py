a386af5ae6de137c1dd7892a3efcf4490a54691b
Ken Sible <ksible@outlook.com>
2026-08-12 16:51:23 -0400

    added meowfacts to InsultBot

--- cogs/insult.py
+++ cogs/insult.py
@@ -13,5 +13,10 @@ class InsultBot(commands.Cog):
         response = requests.get(self.api_url)
         await interaction.response.send_message(f'<@{who.id}> {json.loads(response.text)["insult"]}')
 
+    @app_commands.command(name='meowfacts', description='Send a random cat fact to someone.')
+    async def _meowfacts(self, interaction: discord.Interaction, who: discord.User):
+        response = requests.get('https://meowfacts.herokuapp.com/')
+        await interaction.response.send_message(f'<@{who.id}> {json.loads(response.text)["data"][0]}')
+
 async def setup(bot: commands.Bot):
     await bot.add_cog(InsultBot(bot))
